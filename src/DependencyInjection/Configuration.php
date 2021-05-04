8abb70c5319c3b988722e0a2e631d598c5dbbfd1
IDCT Bartosz Pachołek <bartosz@idct.pl>
2021-05-04 22:41:47 +0200

    Initial release

---
+++ src/DependencyInjection/Configuration.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Praetorian\RefreshTokenManagerBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+final class Configuration implements ConfigurationInterface
+{
+    public function getConfigTreeBuilder(): TreeBuilder
+    {
+        $treeBuilder = new TreeBuilder('praetorian_refresh_token_manager_bundle');
+        //TODO variable token key?
+        return $treeBuilder;
+    }
+}
---
+++ src/PraetorianRefreshTokenManagerBundle.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+namespace Praetorian\RefreshTokenManagerBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+final class PraetorianRefreshTokenManagerBundle extends Bundle
+{
+}
