be0cdf3cae60b622d2840e45112b47d340a2a4c4
IDCT Bartosz Pachołek <bartosz@idct.pl>
2021-05-06 17:34:22 +0200

    remove the version constraint on monolog

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -12,7 +12,6 @@ final class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder(): TreeBuilder
     {
         $treeBuilder = new TreeBuilder('praetorian_refresh_token_manager_bundle');
-        //TODO variable token key?
         return $treeBuilder;
     }
 }
